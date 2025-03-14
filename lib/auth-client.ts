4c0bb5402ae0a466f4597ee24bea414a405bf6d4
Wellington Galvão <wellingtongalvao96@gmail.com>
2025-03-14 17:03:06 -0300

    feat: adição de baseURL

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,3 +1,5 @@
 import { createAuthClient } from "better-auth/react"
 
-export const { signIn, signUp, signOut, useSession } = createAuthClient()
+export const { signIn, signUp, signOut, useSession } = createAuthClient({
+  baseURL: process.env.NEXT_PUBLIC_APP_URL
+})
