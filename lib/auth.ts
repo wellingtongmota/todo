2e9cad6e69f9fc40d33dea6571c27b88831bb389
Wellington Galvão <wellingtongalvao96@gmail.com>
2025-03-14 13:38:29 -0300

    build: implementação better-auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,5 +5,8 @@ import { prisma } from "./prisma"
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql"
-  })
+  }),
+  emailAndPassword: {
+    enabled: true
+  }
 })
