49bdc2115abf3ad5fb565e2be5064663828aeb45
Wellington Galvão <wellingtongalvao96@gmail.com>
2025-03-18 13:22:40 -0300

    refactor: melhorias no middleware

--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,46 @@
-import { NextRequest, NextResponse } from "next/server"
-import { getUrl } from "./lib/get-url"
+import { betterFetch } from "@better-fetch/fetch"
+import { NextResponse, type NextRequest } from "next/server"
+import { Session } from "./types/auth-types"
 
-export function middleware(request: NextRequest) {
-  const token = request.cookies.get("better-auth.session_token")
-  const pathname = request.nextUrl.pathname
+const authRoutes = ["/sign-in", "/sign-up"]
+const passwordRoutes = ["/reset-password", "/forgot-password"]
+const adminRoutes = ["/admin"]
 
-  if ((pathname === "/sign-in" || pathname === "/sign-up") && token) {
-    return NextResponse.redirect(new URL(getUrl("/app")))
+export default async function authMiddleware(request: NextRequest) {
+  const pathName = request.nextUrl.pathname
+  const isAuthRoute = authRoutes.includes(pathName)
+  const isPasswordRoute = passwordRoutes.includes(pathName)
+  const isAdminRoute = adminRoutes.includes(pathName)
+
+  const { data: session } = await betterFetch<Session>(
+    "/api/auth/get-session",
+    {
+      baseURL: process.env.BETTER_AUTH_URL,
+      headers: {
+        //get the cookie from the request
+        cookie: request.headers.get("cookie") || ""
+      }
+    }
+  )
+
+  if (!session) {
+    if (isAuthRoute || isPasswordRoute) {
+      return NextResponse.next()
+    }
+    return NextResponse.redirect(new URL("/sign-in", request.url))
   }
 
-  if (pathname === "/") {
-    return NextResponse.redirect(new URL(getUrl("/app")))
+  if (isAuthRoute || isPasswordRoute) {
+    return NextResponse.redirect(new URL("/app", request.url))
   }
 
-  if (pathname.includes("/app") && !token) {
-    return NextResponse.redirect(new URL(getUrl("/sign-in")))
-  }
+  // if (isAdminRoute && session.user.role !== "admin") {
+  //   return NextResponse.redirect(new URL("/", request.url));
+  // }
 
-  if (pathname.includes("/app") && !token) {
-    return NextResponse.redirect(new URL(getUrl("/sign-in")))
-  }
+  return NextResponse.next()
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"]
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"]
 }
