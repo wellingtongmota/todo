8967a1b93308bdd1a60890b2dc61f14e80dfe684
Wellington Galvão <wellingtongalvao96@gmail.com>
2025-03-24 20:29:39 -0300

    feat: adição da sessão Features

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,11 @@
+import Features from "./_components/features"
 import HeroSection from "./_components/hero-section"
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <Features />
     </main>
   )
 }
